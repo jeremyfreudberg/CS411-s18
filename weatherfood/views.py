5a3bdc4932f3f12627c90717330f1e4d50066e6e
Jeremy Freudberg <jeremyfreudberg@gmail.com>
2018-11-30 13:37:13 -0500

    Login stuff

--- weatherfood/views.py
+++ weatherfood/views.py
@@ -3,6 +3,7 @@ from weatherfood import api
 from weatherfood import application
 
 app = application.app
+google = application.google_login
 
 @app.route('/', methods=['GET'])
 def index():
@@ -13,3 +14,16 @@ def weather():
     current_weather = api.get_weather_pretty(
         flask.request.form.get("zipcode"))
     return flask.render_template('result.html', weather=current_weather)
+
+@app.route('/login_test', methods=['GET'])
+def login():
+    return '<a href="{}">Login with Google</a>'.format(
+        google.authorization_url())
+
+@google.login_success
+def login_success(token, profile):
+    return flask.jsonify(token=token, profile=profile)
+
+@google.login_failure
+def login_failure(e):
+    return flask.jsonify(error=str(e))
