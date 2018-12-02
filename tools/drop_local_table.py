fa69312a2b0b05bf1376316c1c765212ea8583e7
Jeremy Freudberg <jeremyfreudberg@gmail.com>
2018-12-02 16:16:23 -0500

    Script for dropping local dev table

---
+++ tools/drop_local_table.py
@@ -0,0 +1,10 @@
+import boto3
+
+dynamo = boto3.resource('dynamodb',
+                        endpoint_url='http://localhost:8000',
+                        region_name='dummy',
+                        aws_access_key_id='dummy',
+                        aws_secret_access_key='dummy')
+
+t = dynamo.Table('CS411')
+t.delete()
